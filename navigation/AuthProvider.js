14d5c9ad012cd48e9a6c5532cba2c36d8de91845
balaji6666 <balaji.foo404@gmail.com>
2020-10-25 11:33:57 +0530

    Login | SignUp | Onboarding | FirebaseAuth

---
+++ navigation/AuthProvider.js
@@ -0,0 +1,39 @@
+import React, {createContext, useState} from 'react';
+// import auth from '@react-native-firebase/auth';
+import Firebase from '../firebaseConfig';
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        login: async (email, password) => {
+          try {
+            Firebase.auth().signInWithEmailAndPassword(email, password)
+          } catch (e) {
+            console.log(e);
+          }
+        },
+        register: async (email, password) => {
+          try {
+            Firebase.auth().createUserWithEmailAndPassword(email, password)
+          } catch (e) {
+            console.log(e);
+          }
+        },
+        logout: async () => {
+          try {
+            Firebase.auth().signOut();
+          } catch (e) {
+            console.log(e);
+          }
+        },
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
